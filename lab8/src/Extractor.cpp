f9cfe097c1502097ce45c05b3851319239629a25
Josh Kosberg <jkosberg@adobe.com>
2025-03-30 17:23:34 -0500

    Updated to pass more tests

--- lab8/src/Extractor.cpp
+++ lab8/src/Extractor.cpp
@@ -91,53 +91,24 @@ void Extractor::extractConstraints(const InstMapTy &InstMap, Instruction *I)
     Value *StoredValue = SI->getValueOperand();
     Value *Pointer = SI->getPointerOperand();
 
-    // Only process integer types
-    if (Pointer->getType()->isPointerTy() &&
-        Pointer->getType()->getPointerElementType()->isIntegerTy())
-    {
-      addDef(InstMap, Pointer, I); // The pointer being written to is "defined"
-    }
-
-    if (StoredValue->getType()->isIntegerTy())
-    {
-      addUse(InstMap, StoredValue, I); // The value being stored is "used"
-    }
+    addDef(InstMap, Pointer, I);     // The pointer being written to is "defined"
+    addUse(InstMap, StoredValue, I); // The value being stored is "used"
   }
   else if (LoadInst *LI = dyn_cast<LoadInst>(I))
   {
     Value *Pointer = LI->getPointerOperand();
 
-    // Only process integer types
-    if (Pointer->getType()->isPointerTy() &&
-        Pointer->getType()->getPointerElementType()->isIntegerTy())
-    {
-      addUse(InstMap, Pointer, I); // The pointer being read from is "used"
-    }
-
-    if (LI->getType()->isIntegerTy())
-    {
-      addDef(InstMap, LI, I); // The result of the load is "defined"
-    }
+    addUse(InstMap, Pointer, I); // The pointer being read from is "used"
+    addDef(InstMap, LI, I);      // The result of the load is "defined"
   }
   else if (BinaryOperator *BI = dyn_cast<BinaryOperator>(I))
   {
     Value *Op1 = BI->getOperand(0);
     Value *Op2 = BI->getOperand(1);
 
-    // Only process integer types
-    if (Op1->getType()->isIntegerTy())
-    {
-      addUse(InstMap, Op1, I); // Operand 1 is "used"
-    }
-    if (Op2->getType()->isIntegerTy())
-    {
-      addUse(InstMap, Op2, I); // Operand 2 is "used"
-    }
-
-    if (BI->getType()->isIntegerTy())
-    {
-      addDef(InstMap, BI, I); // The result of the operation is "defined"
-    }
+    addUse(InstMap, Op1, I); // Operand 1 is "used"
+    addUse(InstMap, Op2, I); // Operand 2 is "used"
+    addDef(InstMap, BI, I);  // The result of the operation is "defined"
 
     // If the operation is a division, add a fact to the `div` relation
     if ((BI->getOpcode() == Instruction::SDiv || BI->getOpcode() == Instruction::UDiv) &&
@@ -162,49 +133,26 @@ void Extractor::extractConstraints(const InstMapTy &InstMap, Instruction *I)
     {
       addSanitizer(InstMap, I); // Add a fact to the `sanitizer` relation
     }
-    // else
-    // {
-    //   // General function call handling
-    //   addDef(InstMap, CI, I); // Add a `def` relation for the return value
-    //   for (unsigned i = 0; i < CI->getNumArgOperands(); ++i)
-    //   {
-    //     addUse(InstMap, CI->getArgOperand(i), I); // Add `use` facts for arguments
-    //   }
-    // }
+    addDef(InstMap, CI, I); // Add a `def` relation for the return value
+    for (unsigned int i = 0; i < CI->getNumArgOperands(); i++)
+    {
+      addUse(InstMap, CI->getArgOperand(i), I); // Add `use` relations for arguments
+    }
   }
   else if (CastInst *CI = dyn_cast<CastInst>(I))
   {
     Value *Operand = CI->getOperand(0);
 
-    // Only process integer types
-    if (Operand->getType()->isIntegerTy())
-    {
-      addUse(InstMap, Operand, I); // The operand is "used"
-    }
-
-    if (CI->getType()->isIntegerTy())
-    {
-      addDef(InstMap, CI, I); // The result of the cast is "defined"
-    }
+    addUse(InstMap, Operand, I); // The operand is "used"
+    addDef(InstMap, CI, I);      // The result of the cast is "defined"
   }
   else if (CmpInst *CI = dyn_cast<CmpInst>(I))
   {
     Value *Op1 = CI->getOperand(0);
     Value *Op2 = CI->getOperand(1);
 
-    // Only process integer types
-    if (Op1->getType()->isIntegerTy())
-    {
-      addUse(InstMap, Op1, I); // Operand 1 is "used"
-    }
-    if (Op2->getType()->isIntegerTy())
-    {
-      addUse(InstMap, Op2, I); // Operand 2 is "used"
-    }
-
-    if (CI->getType()->isIntegerTy())
-    {
-      addDef(InstMap, CI, I); // The result of the comparison is "defined"
-    }
+    addUse(InstMap, Op1, I); // Operand 1 is "used"
+    addUse(InstMap, Op2, I); // Operand 2 is "used"
+    addDef(InstMap, CI, I);  // The result of the comparison is "defined"
   }
 }
